7cb53b82f56c1be3dba01bd3e30e09a53e115414
Joshua Shannon <retrodaredevil@gmail.com>
2020-11-07 00:44:24 -0600

    Initial commit using gdx-liftoff project creator

---
+++ core/src/main/java/me/retrodaredevil/game/lunarperiphery/LunarMain.java
@@ -0,0 +1,7 @@
+package me.retrodaredevil.game.lunarperiphery;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class LunarMain extends ApplicationAdapter {
+}
---
+++ lwjgl3/src/main/java/me/retrodaredevil/game/lunarperiphery/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package me.retrodaredevil.game.lunarperiphery.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import me.retrodaredevil.game.lunarperiphery.LunarMain;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		return new Lwjgl3Application(new LunarMain(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("lunar-periphery");
+		configuration.setWindowedMode(640, 480);
+		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		return configuration;
+	}
+}
